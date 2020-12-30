32712f540535ebf6fc45f242c1c1d8724d7ca0b4
Kailairajan Luxithan <kailairajan@et.esiea.fr>
2020-12-30 03:19:15 +0100

    Buttons Activation

--- app/src/main/java/com/example/android4a/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/android4a/presentation/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.android4a.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.Observer
 import com.example.android4a.R
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
+
 
 class MainActivity : AppCompatActivity() {
     val mainViewModel: MainViewModel by inject()
@@ -15,12 +20,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mainViewModel.loginLiveData.observe(this, Observer {
-            when(it){
+            when (it) {
                 is LoginSuccess -> {
                     MaterialAlertDialogBuilder(this)
                         .setTitle("Succès")
                         .setMessage("Compte reconnu")
-                        .setPositiveButton("OK"){ dialog, which ->
+                        .setPositiveButton("OK") { dialog, which ->
                             dialog.dismiss()
                         }
                         .show()
@@ -29,7 +34,7 @@ class MainActivity : AppCompatActivity() {
                     MaterialAlertDialogBuilder(this)
                         .setTitle("Erreur")
                         .setMessage("Compte inconnu")
-                        .setPositiveButton("OK"){ dialog, which ->
+                        .setPositiveButton("OK") { dialog, which ->
                             dialog.dismiss()
                         }
                         .show()
@@ -37,7 +42,7 @@ class MainActivity : AppCompatActivity() {
                     MaterialAlertDialogBuilder(this)
                         .setTitle("Erreur")
                         .setMessage("Mot de passe inconnu")
-                        .setPositiveButton("OK"){ dialog, which ->
+                        .setPositiveButton("OK") { dialog, which ->
                             dialog.dismiss()
                         }
                         .show()
@@ -45,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                     MaterialAlertDialogBuilder(this)
                         .setTitle("Succès")
                         .setMessage("Votre compte a bien été crée")
-                        .setPositiveButton("OK"){ dialog, which ->
+                        .setPositiveButton("OK") { dialog, which ->
                             dialog.dismiss()
                         }
                         .show()
@@ -53,19 +58,40 @@ class MainActivity : AppCompatActivity() {
                     MaterialAlertDialogBuilder(this)
                         .setTitle("Erreur")
                         .setMessage("Ce Login est déjà utilisé")
-                        .setPositiveButton("OK"){ dialog, which ->
+                        .setPositiveButton("OK") { dialog, which ->
                             dialog.dismiss()
                         }
                         .show()
             }
         })
 
+        login_button.isEnabled = false
+        create_account_button.isEnabled = false
+        login_edit.doAfterTextChanged{
+            login_button.isEnabled =
+                !(login_edit.text.toString() == "" || password_edit.text.toString() == "")
+            create_account_button.isEnabled =
+                !(login_edit.text.toString() == "" || password_edit.text.toString() == "")
+        }
+        password_edit.doAfterTextChanged{
+            login_button.isEnabled =
+                !(login_edit.text.toString() == "" || password_edit.text.toString() == "")
+            create_account_button.isEnabled =
+                !(login_edit.text.toString() == "" || password_edit.text.toString() == "")
+        }
+
         login_button.setOnClickListener(){
-            mainViewModel.onClickedLogin(login_edit.text.toString().trim(), password_edit.text.toString())
+            mainViewModel.onClickedLogin(
+                login_edit.text.toString().trim(),
+                password_edit.text.toString()
+            )
         }
 
         create_account_button.setOnClickListener(){
-            mainViewModel.onClickedCreate(login_edit.text.toString().trim(), password_edit.text.toString())
+            mainViewModel.onClickedCreate(
+                login_edit.text.toString().trim(),
+                password_edit.text.toString()
+            )
         }
 
     }
